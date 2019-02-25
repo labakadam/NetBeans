71b1c5594ad823de545a6c63c24456b006d73c21
Adam Labak <labakadam1993@gmail.com>
2019-02-25 14:53:24 +0100

    Hibernate i završni rad

---
+++ HibernateORM_2/src/safeflyeu1/Start.java
@@ -0,0 +1,14 @@
+package safeflyeu1;
+
+// domaća zadaća:
+
+import safeflyeu.pomocno.HibernateUtil;
+
+//upogoniti Hibernate prateći https://examples.javacodegeeks.com/enterprise-java/hibernate/hibernate-maven-example/
+public class Start {
+
+    public static void main(String[] args) {
+        HibernateUtil.getSession();
+    }
+    
+}
